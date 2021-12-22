d433b0d7f2eb16334683c67e1d08f4b77e95348d
error-rgl <rglcnd@gmail.com>
2021-12-22 02:40:52 +0100

    [:Update:] all content the file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-100, 100)
+number = random.randint(-10000, 10000)
 
 if number < 0:
-	last_digit = int(repr(number)[-1]) * -1
+    convert = number * -1
+    last_digit = (convert % 10) * -1
 elif number >= 0:
-	last_digit = int(repr(number)[-1])
-
-print("Last digit of {} is {}".format(number, last_digit), end=' ')
+    last_digit = number % 10
 
 if last_digit > 5:
-	print("and is greater than 5")
+    ms = "and is greater than 5"
 elif last_digit == 0:
-	print("and is 0")
+    ms = "and is 0"
 elif last_digit < 6:
-	print("and is less than 6 and not 0")
+    ms = "and is less than 6 and not 0"
 
+print("Last digit of {} is {} {}".format(number, last_digit, ms), end=' ')
