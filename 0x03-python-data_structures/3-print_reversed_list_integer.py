d38381b78aba005b3bccbcc20a8260a04b5e44b0
error-rgl <rglcnd@gmail.com>
2022-01-05 05:47:43 +0100

    [:Edit:] change the for conditional on line 5

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
         return None
-    for i in range(1, len(my_list), -1):
+    for i in reversed(my_list):
         print("{:d}".format(i))
