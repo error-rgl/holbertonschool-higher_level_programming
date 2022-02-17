817c746a3c3df10e172d21d01bc997d5d9087e49
error-rgl <rglcnd@gmail.com>
2022-02-17 06:32:31 +0100

    [:Update:] Add new validations with new structure.

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,58 +1,50 @@
 #!/usr/bin/python3
-"""Class defines square """
+"""Class defines square"""
 
 
 class Square:
-    ''' A clas that defined a square by its size and position'''
+    """ A class that defines a square by its size
+    """
     def __init__(self, size=0, position=(0, 0)):
-        """ Method to inicialize the square object.
+        """ Method to initialize the square object
         """
-        self.__size = size
-        self.__position = position
-
-    def area(self):
-        """ Method that returns the square are of the object
-        """
-        return(self.__size ** 2)
-
-    def my_print(self):
-        """ Method that prints in stdout the square with the character #.
-        """
-        if self.__size == 0:
-            print()
-            return
-        for i in range(self.__position[1]):
-            print()
-        for j in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """ Method to returns the size value.
+        """ Method to returns the size value
         """
-        return (self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value):
-        """ Method to set the size value of the square object.
+        """ Method to set the size value of the square object
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     @property
     def position(self):
-        """ Method to returns the position value.
+        """ Method that returns the position value
         """
-        return (self.__position)
+        return self.__position
 
     @position.setter
     def position(self, value):
-        """ Method to set the position value of the square object.
+        """ Method that sets the position value of a square object
         """
-        if type(value) is not tuple\
-            and type(value[0]) is not int\
-                and type(value[1]) is not int:
+        if not isinstance(value, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
