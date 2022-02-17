33d45e03369d4f012c3762826f79fec1a1853e7f
error-rgl <rglcnd@gmail.com>
2022-02-17 06:00:07 +0100

    [:Fix:] Edit all content of file

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,32 +6,26 @@ class Square:
     def __init__(self, size=0):
         """ Method to initialize the square object
         """
-        self.size = size
+        self.__size = size
 
     @property
     def size(self):
-        """ Method to returns the size value.
-        """
-        return (self.__size)
+        return(self.__size)
 
     @size.setter
     def size(self, value):
-        """ Method to set the size value of the square object.
-        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        """ Method that returns the square are of the object
-        """
-        return(self.__size ** 2)
+        """calculates and returns current square area"""
+        return(self.__size * self.__size)
 
     def my_print(self):
-        """ that prints in stdout the square with the character #.
-        """
+        """prints square of size self.__size using #"""
         if self.__size > 0:
             for column in range(self.__size):
                 for row in range(self.__size):
