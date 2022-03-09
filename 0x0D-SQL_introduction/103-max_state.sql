2a6e65741f6cd5363cae1dd278e564cc0f1c78ef
error-rgl <rglcnd@gmail.com>
2022-03-09 20:27:09 +0100

    [: Feature :] Script that shows the maximum temperature in a table

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,6 @@
+-- Script that shows the maximum temperature by state
+-- Query that shows the maximum temperature of each state of the temperatures table
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+LIMIT 3;
