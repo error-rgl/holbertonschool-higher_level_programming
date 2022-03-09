b687d1c4743441a23321cf72f45f5016acc2cddf
error-rgl <rglcnd@gmail.com>
2022-03-09 18:51:23 +0100

    [:Feature:] Script that lists all records that have a score >= 10

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Query that lists all records with the following 
+-- condition(score >= 10 ) in the second_table of a db
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
