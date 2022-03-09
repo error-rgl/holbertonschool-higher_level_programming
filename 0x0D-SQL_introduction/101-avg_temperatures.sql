2b7310313aed7139330442bdf9a6ab97c916a731
error-rgl <rglcnd@gmail.com>
2022-03-09 20:35:24 +0100

    [: Fix :] Change column header or alias

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- Script that displays the average temperature
 -- Query that shows the average temperature by city arranged in a DESC way.
-SELECT city, AVG(value) AS avg_tmp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
-ORDER BY avg_tmp DESC;
+ORDER BY avg_temp DESC;
