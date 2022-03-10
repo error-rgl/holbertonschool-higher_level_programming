2c0af731e2833846ba6c0d4c11579d1417e0463d
error-rgl <rglcnd@gmail.com>
2022-03-10 05:51:23 +0100

    [: FEATURE :] Script that lists all privileges of two users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Script that lists all privileges of the MySQL users
+-- Query that lists all privileges (GRANT) of MySQL users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost'; 
