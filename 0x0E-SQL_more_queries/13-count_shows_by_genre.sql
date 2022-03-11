689907c1a9d1e490a31c9dbe3095eba1aba29a01
error-rgl <rglcnd@gmail.com>
2022-03-11 05:37:32 +0100

    [: Fix :] New sintax to join tables

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
 -- Script that lists genres and number of shows.
--- Query to join tables.
-SELECT g.name AS genre, COUNT(t.show_id) AS number_of_shows
-	FROM tv_genres AS g
-	LEFT JOIN tv_show_genres AS t
-	ON g.id = t.genre_id
-	GROUP BY g.id
-	ORDER BY number_of_shows DESC;
+-- Query that joins tables.
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_show_genres
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC, tv_genres.id ASC;
