e497bf62a6cde4836e9976cf0c27e5c247ee318c
error-rgl <rglcnd@gmail.com>
2022-03-11 05:22:20 +0100

    [: Fix :] Fix typo in SELECT statement

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- Script that lists all cities contained in the database.
 -- Query to join 2 tables (cities and states).
-SELECT tv_shows.title, tv_show_genres.genre.id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id is NULL
