46be97d0faad87ec6307733e3dccdd245cd2dac8
error-rgl <rglcnd@gmail.com>
2022-03-16 03:32:05 +0100

    [Fix]: Typo error on line 4

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Script that displays the average temperature.
 -- Query showing the average temperature per city during 
 -- 	July and August sorted by temperature (descending).
-SELECT city, AVG(value) AS avg_tmp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 WHERE month = 7 OR month = 8
 GROUP BY city
