bddc724f8d22626f726938e3e544eb00455f2714
error-rgl <rglcnd@gmail.com>
2022-03-26 05:26:24 +0100

    [Fix]: edit all content of file

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-def lookup(obj):
-    """ Function that returns the list of available attributes
-        and methods of an object
-    Args:
-        obj: instance of the class
-    Returns:
-        List of attributes
-    """
+"""Methods module"""
 
+
+def lookup(obj):
+    """
+    function that returns the list of available
+    attributes and methods of an object
+    """
     return dir(obj)
