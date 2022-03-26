571b4aced743a106ee45d5507634481f81a01011
error-rgl <rglcnd@gmail.com>
2022-03-26 04:36:57 +0100

    [Fix]: edit all content of file

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-""" This is a module that containts a clas that avoids
-dynmaically created attributes.
+"""
+This is a module that containts a clas that avoids
+dynmaically created attributes
 """
 
 
 class LockedClass:
-    __slots__ = "first_name"
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """ Init method """
+        pass
