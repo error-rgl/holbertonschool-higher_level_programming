884eb233c41f13bda3c121e3c1d477ff64f6ec67
error-rgl <rglcnd@gmail.com>
2022-03-26 05:24:52 +0100

    [Fix]: edit all content of file

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-class MyList(list):
-    """ Class that inherits the attributes references of class list
-    Args:
-        list: class list
-    """
+"""Class definition"""
 
+
+class MyList(list):
+    """Class MyList inherited"""
     def print_sorted(self):
-        """ Method that prints the sorted list """
-        l_sorted = self.copy()
-        l_sorted.sort()
-        print(l_sorted)
+        """prints sorted list"""
+        print(sorted(self))
