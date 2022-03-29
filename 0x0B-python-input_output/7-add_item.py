bbe2aff15d7cb7edeb162df339c1cb4619d08193
error-rgl <rglcnd@gmail.com>
2022-03-30 01:01:14 +0200

    [Fix]: that name of files import

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """ Module that adds all arguments to a Python list, and then
 save them to a file
@@ -7,8 +6,8 @@ import sys
 import os.path
 
 
-save_file = __import__('7-save_to_json_file').save_to_json_file
-load_file = __import__('8-load_from_json_file').load_from_json_file
+save_file = __import__('5-save_to_json_file').save_to_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
 
 my_list = []
 if os.path.exists("add_item.json"):
