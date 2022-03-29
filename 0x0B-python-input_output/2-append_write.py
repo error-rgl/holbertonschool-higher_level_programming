406f4f99de7c15cfb740ab7a35821822e2149d8f
error-rgl <rglcnd@gmail.com>
2022-03-30 00:17:20 +0200

    [Fix]: function that appends a string at the end of a text file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,18 +7,9 @@ def append_write(filename="", text=""):
     """ Function that reads from a file and prints its number of lines
     Args:
         filename: filename
-        text: number of lines to print
+        text: append write
     Raises
         Exception: when the file can be opened
     """
 with open(filename, 'r' encoding="utf-8") as f:
-    if text <= 0:
-        read_data = f.read()
-        print(read_data, end='')
-    else:
-        num_line = 0
-        for line in f:
-            print(line, end='')
-            num_line +=1
-            if num_line == text:
-                break
+    return(f.write(text))
