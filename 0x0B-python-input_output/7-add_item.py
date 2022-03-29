127b8bc1680b9d9690da5d12b750f914d5b54524
error-rgl <rglcnd@gmail.com>
2022-03-30 01:21:48 +0200

    [Update]: new scripts add arguments to a list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,21 +2,22 @@
 """ Module that adds all arguments to a Python list, and then
 save them to a file
 """
-import sys
+from sys import argv
 import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-list_arg = []
+filename = "add_item.json"
 
 try:
-    with open("add_item.json", "r") as file:
-        list_arg = json.loads(file.read())
-except FileNotFoundError:
-    pass
-finally:
-    with open("add_item.json", "w") as file:
-        for pos in range(1, len(sys.argv)):
-            list_arg.append(sys.argv[pos])
-        file.write(json.dumps(list_arg))
+    f = "add_item.json"
+    obj = load_from_json_file(filename)
+except Exception:
+    obj = []
+
+for i in range(1, len(argv)):
+    obj.append(argv[i])
+
+with open(filename, 'w') as f:
+    json.dump(obj, f)
