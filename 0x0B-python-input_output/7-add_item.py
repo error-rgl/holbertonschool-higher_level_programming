5e43b4d901fce8c83780e8d46eeb1508c1d7051e
error-rgl <rglcnd@gmail.com>
2022-03-30 01:10:30 +0200

    Update file name to be a string

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,18 +2,16 @@
 """ Module that adds all arguments to a Python list, and then
 save them to a file
 """
-import sys
-import os.path
+from sys import argv
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-save_file = __import__('5-save_to_json_file').save_to_json_file
-load_file = __import__('6-load_from_json_file').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-my_list = []
-if os.path.exists("add_item.json"):
-    my_list = load_file("add_item.json")
+try:
+    arglist = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    arglist = []
 
-for arg in sys.argv[1:]:
-    my_list.append(arg)
-
-save_file(my_list, "add_item.json")
+arglist += argv[1:]
+save_to_json_file(arglist, "add_item.json")
