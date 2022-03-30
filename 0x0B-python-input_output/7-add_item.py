33e410575697c9e3ab1ba7fd8d884fcb23fe6f0c
error-rgl <rglcnd@gmail.com>
2022-03-30 04:10:54 +0200

    [Fix]: optimize code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-""" Module that adds all arguments to a Python list, and then
-save them to a file
+""" Use the module for adds all argument to a Python list,
+    and the save them to a file
 """
-
-from sys import argv
+import sys
 import json
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-filename = "add_item.json"
+my_list = []
 
 try:
-    f = "add_item.json"
-    obj = load_from_json_file(filename)
-except Exception:
-    obj = []
-
-for i in range(1, len(argv)):
-    obj.append(argv[i])
-
-with open(filename, 'w') as f:
-    json.dump(obj, f)
+    with open("add_item.json", "r") as file:
+        my_list = json.loads(file.read())
+except FileNotFoundError:
+    pass
+finally:
+    with open("add_item.json", "w") as file:
+        for pos in range(1, len(sys.argv)):
+            my_list.append(sys.argv[pos])
+        file.write(json.dumps(my_list))
