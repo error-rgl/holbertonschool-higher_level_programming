f78cf0c97939e2e7205ef56cd3856e404d1cf0e4
error-rgl <rglcnd@gmail.com>
2022-04-05 19:08:53 +0200

    [Fix]: use of 'for' loop to while

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,8 @@ let myNum = parseInt(process.argv[2]);
 if (Number.isNaN(myNum)) {
 	console.log('Missing number of occurrences');
 } else {
-	for (let i = 0; i <= myNum; i++) {
+	while (myNum > 0) {
 		console.log('C is fun');
+		myNum--;
 	}
 }
