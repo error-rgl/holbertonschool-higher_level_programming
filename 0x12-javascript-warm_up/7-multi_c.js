2d614ec8a99f592a447fd7c38fcce856a52555d5
error-rgl <rglcnd@gmail.com>
2022-04-05 19:35:44 +0200

    [Fix]: the code to the ES6 standard

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 let myNum = parseInt(process.argv[2]);
 if (Number.isNaN(myNum)) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	while (myNum > 0) {
-		console.log('C is fun');
-		myNum--;
-	}
+  while (myNum > 0) {
+    console.log('C is fun');
+    myNum--;
+  }
 }
