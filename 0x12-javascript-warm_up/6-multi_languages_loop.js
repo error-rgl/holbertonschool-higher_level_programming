837889e0412489f5c0b1d20de14828df49599fac
error-rgl <rglcnd@gmail.com>
2022-04-05 19:42:55 +0200

    [Fix]: the code to the ES6 standard

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myList  = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const myList = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < myList.length; i++) {
-	console.log(myList[i]);
+  console.log(myList[i]);
 }
