6e1e20298c309e49a42a7825ee78dff3faec0a91
error-rgl <rglcnd@gmail.com>
2022-04-06 06:31:00 +0200

    [Update]: all content of file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,17 @@
-#!/usr/bin/node
-const SquareP = require('./5-square');
+!/usr/bin/node
+const prevSquare = require('./5-square');
 
-class Square extends SquareP {
-  charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
+class Square extends prevSquare {
+  // Method to print Square instance using given character or 'X'
+  charPrint (c = 'X') {
+    let row = '';
+    let i = 0;
+    for (i = 0; i < this.width; i++) {
+      row += c;
     }
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += c;
-      }
-      console.log(s);
+    for (i = 0; i < this.height; i++) {
+      console.log(row);
     }
   }
 }
-
 module.exports = Square;
