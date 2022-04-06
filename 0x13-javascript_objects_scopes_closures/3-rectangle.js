e7051e3387f8acf3f9e7aa351c34414c9483b72c
error-rgl <rglcnd@gmail.com>
2022-04-06 06:14:31 +0200

    [Fix]: delete blank line on line 15

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -11,7 +11,7 @@ class Rectangle {
   print () {
     for (let i = 0; i < this.height; i++) {
       let str = '';
-      for (let j  = 0; j < this.width; j++) {
+      for (let j = 0; j < this.width; j++) {
         str += 'X';
       }
       console.log(str);
