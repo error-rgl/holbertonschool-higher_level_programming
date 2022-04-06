edf9736d12b4f582820482f6bafff445b7184e1e
error-rgl <rglcnd@gmail.com>
2022-04-06 06:01:01 +0200

    [Feature]: add class that has a method which uses its two attrs

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // Method to print the rectangle using 'X'
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let str = '';
+      for (let j  = 0; j < this.width; j++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+}
+
+module.exports = Rectangle;
