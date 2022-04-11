427883c234639d8cf45c4f74d5e542af68577cc5
error-rgl <rglcnd@gmail.com>
2022-04-11 17:27:37 +0200

    [Update]: adding the requirement to sort the result ascending by id

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states;")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
     query = cur.fetchall()
 
     for states in query:
