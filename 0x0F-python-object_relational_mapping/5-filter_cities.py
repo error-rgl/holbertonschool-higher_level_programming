ae9e7d23863db1ffe1f6d85384bce18b20da8edd
error-rgl <rglcnd@gmail.com>
2022-04-11 20:27:46 +0200

    [Feature]: adding a script that lists all cities of a state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" lists all cities from the database
+matching state name passed in as argument using MySQLdb
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name
+    FROM cities JOIN states ON cities.state_id = states.id
+    WHERE states.name = '{}';""".format(sys.argv[4]))
+    query = cur.fetchall()
+
+    print(", ".JOIN([state[1] for states in query]))
+
+    cur.close()
+    db.close()
