dc94ec60f3958e950c941473869100f6b81435e9
error-rgl <rglcnd@gmail.com>
2022-04-11 20:17:59 +0200

    [Fix]: edit that execute Sql query

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,12 +5,16 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
+    # Open database connection
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name states.name \
-    FROM cities JOIN states ON cities.state_id = states.id;")
+    # execute SQL query using execute() method.
+    cur.execute("""SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id;
+    """)
     query = cur.fetchall()
 
     for states in query:
