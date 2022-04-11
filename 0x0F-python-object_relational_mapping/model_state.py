75ee8681391bcb5d728b4b2c9070a9ac9b6ff408
error-rgl <rglcnd@gmail.com>
2022-04-11 21:16:38 +0200

    [Feature]: adding a py file that containts the State class

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Defines State class that inherits from Base = declarative_base()
+and links to MySQL table states using SQLAlchemy
+"""
+from SQLAlchemy import Column, Integer, String, MetaData
+from SQLAlchemy.ext.declarative import declarative_base
+
+mydata = MetaData()
+Base = declarative_base(metadata=mydata)
+
+
+class State(Base):
+    """ Defines State class that links to MySQL table 'states'
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
