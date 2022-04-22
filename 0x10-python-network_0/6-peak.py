6eef0aad88f65d3cbe5281aa9a18e1fab54e55af
error-rgl <rglcnd@gmail.com>
2022-04-22 07:17:07 +0200

    [Update]: to check peak_high before peak_low recursively

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
-""" Finds a peak inside a list """
+""" Defines function to find peak in list of unsorted integers
+"""
 
 
 def find_peak(list_of_integers):
-    if list_of_integers == []:
+    """
+    finds a peak in a list of unsorted ints
+    """
+    if list_of_integers is None or list_of_integers == []:
         return None
-
-    length = len(list_of_integers)
-    mid = int(length / 2)
-    li = list_of_integers
-
-    if mid - 1 < 0 and mid + 1 >= length:
-        return li[mid]
-    elif mid - 1 < 0:
-        return li[mid] if li[mid] > li[mid + 1] else li[mid + 1]
-    elif mid + 1 >= length:
-        return li[mid] if li[mid] > li[mid - 1] else li[mid - 1]
-
-    if li[mid - 1] < li[mid] > li[mid + 1]:
-        return li[mid]
-
-    if li[mid + 1] > li[mid - 1]:
-        return find_peak(li[mid:])
-    return find_peak(li[:mid])
+    mid = int(len(list_of_integers) / 2)
+    if len(list_of_integers) % 2 == 0:
+        mid = mid - 1
+    middle = list_of_integers[mid]
+    low = list_of_integers[0]
+    high = list_of_integers[-1]
+    if middle is low and middle is high:
+        return middle
+    if middle is low and middle > list_of_integers[mid + 1]:
+        return middle
+    if middle > list_of_integers[mid + 1]:
+        if middle > list_of_integers[mid - 1]:
+            return middle
+        else:
+            peak_low = find_peak(list_of_integers[:mid])
+            if peak_low is not None:
+                return peak_low
+    else:
+        peak_high = find_peak(list_of_integers[(mid + 1):])
+        if peak_high is not None:
+            return peak_high
