b6d11ab3817ee0da8b89c348a4c80573021d709b
error-rgl <rglcnd@gmail.com>
2022-04-23 06:55:00 +0200

    [Feature]: adding a script that displays the value of a header var

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script that sends a request to the URL and
+displays the value of a variable in the response header
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    
+    if argv[1]:
+        resp = requests.get(argv[1])
+        print(resp.headers.get('X-Request-Id'))
