c97ccbe47e4057301352687cc4cc4ab082c86530
error-rgl <rglcnd@gmail.com>
2022-09-14 23:29:27 -0500

    [Feature ] scripts that reads and prints the contentof a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// reads and prints the content of a file.
+// Include the fs module
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', function (err, content) {
+    if (content === undefined) {
+        console.log(err);
+    } else {
+        console.log(content);
+    }
+});
