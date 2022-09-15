9fc29afdd3820d125b6613181dcb004296ebc4af
error-rgl <rglcnd@gmail.com>
2022-09-15 00:01:08 -0500

    [Update]: align code to semi standard style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// reads and prints the content of a file.
+// reads and prints  the content of a file.
 // Include the fs module
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', function (err, content) {
