ca3d00841b8cb66f028590a63505d5bb1e169300
error-rgl <rglcnd@gmail.com>
2022-09-14 23:56:55 -0500

    [Fix]:

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 // Include the fs module
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', function (err, content) {
-    if (content === undefined) {
-        console.log(err);
-    } else {
-        console.log(content);
-    }
+  if (content === undefined) {
+    console.log(err);
+  } else {
+    console.log(content);
+  }
 });
