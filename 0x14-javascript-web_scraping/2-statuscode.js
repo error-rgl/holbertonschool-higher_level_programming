9e0becd8a61962df3798273e2f74912f147fd499
error-rgl <rglcnd@gmail.com>
2022-10-05 16:07:29 -0500

    [Fixed]: a typo on line 6, which takes an argument

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,8 @@
 // displays the status code of a GET request.
 const axios = require('axios');
 
-axios.get(process[2])
+axios
+  .get(process.argv[2])
   .then(res => {
     console.log('code:', res.status);
   })
