e7bb7be443fbba99a1e5af710ca1e3bf97d07309
error-rgl <rglcnd@gmail.com>
2022-10-05 18:31:38 -0500

    [Feature]: adding script that prints characters in a movie

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// prints the characters present in a Star Wars
+// Movie... same order
+const axios = require('axios');
+
+const getChar = async (url) => {
+  const name = await axios.get(url);
+  return (name.data.name);
+};
+
+const getAll = async (url) => {
+  const response = await axios.get(url);
+  for (const character of response.data.characters) {
+    const newCaracter = await getChar(character);
+    console.log(newCaracter);
+  }
+};
+
+getAll(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`);
